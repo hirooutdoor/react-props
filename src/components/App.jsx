9fdb43384210c7f0a7053d04253f6513e09a1476
hiro <>
2021-05-10 05:42:04 +0900

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Card from "./Card";
+
+function App() {
+  return(
+    <div>
+      <h1>My Contacts</h1>
+      <Card />
+      <Card />
+      <Card />
+    </div>
+    
+  );
+}
+
+
+export default App;
